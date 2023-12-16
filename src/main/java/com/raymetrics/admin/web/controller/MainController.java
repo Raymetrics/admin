c89f45c677734df2d233cafc642a9353c6850880
eunji <cije0504@naver.com>
2023-12-16 16:26:57 +0900

    sout테스트

--- src/main/java/com/raymetrics/admin/web/controller/MainController.java
+++ src/main/java/com/raymetrics/admin/web/controller/MainController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MainController {
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login(){
+        System.out.println(">>>>>>>>>>>>>>>>>>>로그인 들어옴");
         return "/main/login";
     }
 
